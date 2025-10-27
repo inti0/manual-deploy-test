8a39208c208136f54c8c35e945f819ae20b17293
inti0 <sonin223@gmail.com>
2025-10-27 15:48:25 +0900

    10/27 학습

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.5.7"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com"
+version = "0.0.1-SNAPSHOT"
+description = "s3example"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(25)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(platform("software.amazon.awssdk:bom:2.24.0"))
+    implementation("software.amazon.awssdk:s3")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/s3example/HomeController.java
@@ -0,0 +1,92 @@
+package com.s3example;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.swing.Spring;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+@RestController
+public class HomeController {
+
+    private final S3Client s3Client;
+
+    private static final String BUCKET_NAME = "dev-bucket-inti0-1";
+    private static final String REGION = "ap-northeast-2";
+    private static final String IMG_DIR_NAME = "img1";
+
+    public HomeController(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public static String getS3FileUrl(String fileName) {
+        return "https://" + BUCKET_NAME + ".s3." + REGION + ".amazonaws.com/" + fileName;
+    }
+
+    @GetMapping("/")
+    public List<String> home() {
+        List<Bucket> bucketList = s3Client.listBuckets().buckets();
+        return bucketList.stream().map(Bucket::name).collect(Collectors.toList());
+    }
+
+    @GetMapping("/upload")
+    public String upload() {
+        return """
+                <form action="/upload" method="post" enctype="multipart/form-data">
+                    <input type="file" name="file" accept="image/*">
+                    <input type="submit" value="Upload">
+                </form>
+                """;
+    }
+
+    @PostMapping("/upload")
+    @ResponseBody
+    public String handleFileUpload(MultipartFile file) throws IOException {
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(BUCKET_NAME)
+                .key(IMG_DIR_NAME + "/" + file.getOriginalFilename())
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(putObjectRequest,
+                RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+
+        return """
+                <img src="%s">
+                <hr>
+                <div>업로드 완료</div>
+                """.formatted(getS3FileUrl(IMG_DIR_NAME + "/" + file.getOriginalFilename()));
+    }
+
+    @GetMapping("/deleteFile")
+    public String showDeleteFile() {
+        return """
+                <form action="/deleteFile" method="post">
+                    <input type="text" name="fileName">
+                    <input type="submit" value="delete">
+                </form>
+                """;
+    }
+
+    @PostMapping("/deleteFile")
+    @ResponseBody
+    public String deleteFile(String fileName) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(BUCKET_NAME)
+                .key(IMG_DIR_NAME + "/" + fileName)
+                .build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+        return "파일이 삭제되었습니다.";
+    }
+}
